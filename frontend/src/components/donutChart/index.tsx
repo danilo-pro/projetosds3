d7f474ef80ceb1f8893db85661b7b0850fb4fa0d
Danilo Perez <daniloperez2016@outlook.com>
2021-08-24 21:31:12 -0300

    Static charts

---
+++ frontend/src/components/donutChart/index.tsx
@@ -0,0 +1,27 @@
+import Chart from 'react-apexcharts';
+
+const DonutChart = () => {
+
+    const mockData = {
+        series: [477138, 499928, 444867, 220426, 473088],
+        labels: ['Thor', 'Capitão América', 'Feiticeira Escarlate', 'Doutor Estranho', 'Homem de Ferro']
+    }
+    
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+    
+    
+    return (
+        <Chart 
+            options={{ ...options, labels: mockData.labels }}
+            series={mockData.series}
+            type="donut"
+            height="240"
+        />
+    );
+}
+
+export default DonutChart;
